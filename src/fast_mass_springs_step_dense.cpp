01020f95c2e4bffc92518acce4c490dac42d9858
Winnerrang <leo.hanxu@mail.utoronto.ca>
2022-12-06 18:20:26 -0500

    finish the draft for fast_mass_springs_step_dense

--- src/fast_mass_springs_step_dense.cpp
+++ src/fast_mass_springs_step_dense.cpp
@@ -18,11 +18,29 @@ void fast_mass_springs_step_dense(
   Eigen::MatrixXd & Unext)
 {
   //////////////////////////////////////////////////////////////////////////////
-  // Replace with your code
-  for(int iter = 0;iter < 50;iter++)
-  {
-    const Eigen::MatrixXd l = Ucur;
-    Unext = prefactorization.solve(l);
-  }
+  // Replace with your code 
+
+    double w = 1e10;
+    Eigen::MatrixXd cur_p(Ucur), prev_p(Uprev), next_p;
+
+    for (int iter = 0; iter < 50; iter++) {
+        Eigen::MatrixXd d = Eigen::MatrixXd::Zero(A.rows(), Ucur.cols());
+        for (int spring_idx = 0; spring_idx < E.rows(); spring_idx++) {
+            auto spring = Ucur.row(cur_p(spring_idx, 0)) - cur_p.row(E(spring_idx, 1));
+            d.row(spring_idx) = spring.normalized() * r(spring_idx);
+        }
+
+        //std::cout << d << std::endl;
+
+        const Eigen::MatrixXd y = k * A.transpose() * d +
+            1 / (delta_t * delta_t) * M * (2 * cur_p - prev_p) +
+            fext +
+            w * C.transpose() * C * V;
+        next_p = prefactorization.solve(y);
+        prev_p = cur_p;
+        cur_p = next_p;
+    }
+    Unext = next_p;
+    
   //////////////////////////////////////////////////////////////////////////////
 }
